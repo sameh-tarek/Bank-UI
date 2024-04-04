48dc4565d5b551073e9e8114a9fa6597c821de54
sameh-tarek <tarksame7@gmail.com>
2024-04-04 14:30:41 +0200

    todo

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,18 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient) { }
 
-  login(email: string, password: string): Observable<any> {
-    return this.http.post<any>('http://localhost:8081/bank/auth/authenticate', { email, password });
-  }
-
-  register(name: string, email: string, password: string): Observable<any> {
-    return this.http.post<any>('http://localhost:8081/bank/auth/register', { name, email, password });
-  }
 }
