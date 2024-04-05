29233b5e3e0cdb8c60ebfd12507782791d6dcec0
sameh-tarek <tarksame7@gmail.com>
2024-04-05 17:22:02 +0200

    Auto-generated commit

--- src/app/models/accountLogin/account-login.ts
+++
@@ -1,4 +0,0 @@
-export class AccountLogin {
-  constructor(public email: string, public password: string) {
-  }
-}
--- src/app/models/accountRegister/account-register.ts
+++
@@ -1,4 +0,0 @@
-export class AccountRegister {
-  constructor(public name: string, public email: string, public password: string) {
-  }
-}
